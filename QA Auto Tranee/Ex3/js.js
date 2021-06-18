488e2ba156bf78379ae1561242c9de8bb1276e04
Deuce368 <84134212+Deuce368@users.noreply.github.com>
2021-06-18 15:23:59 +0300

    Bratikov
    Bratikov

---
+++ QA Auto Tranee/Ex3/js.js
@@ -0,0 +1,7 @@
+let number = [33, 22, 43, 44, 85, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 21, 24, 30, 27, 99, 100];
+number.forEach(function (ctr) {
+    if (ctr % 3 === 0) {
+        console.log(ctr);
+    }
+}
+)
